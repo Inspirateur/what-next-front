e99bb39989b9c55daa345e2000050b854d340693
Téo ORTHLIEB <teo.orthlieb@kaizen-solutions.net>
2024-06-04 19:06:32 +0200

    v0 working

---
+++ index.js
@@ -0,0 +1,55 @@
+const back_url = "http://127.0.0.1:8000";
+let curr_oeuvre = null;
+
+window.onload = async function() {
+    // get the media
+    const media = await (await fetch(back_url+"/media")).json();
+    let media_select = document.getElementById("media-select");
+    for (const medium of media) {
+        let option = document.createElement("option");
+        option.value = medium;
+        option.innerText = medium;
+        media_select.appendChild(option);
+    }
+    // get a reco
+    await get_reco();
+};
+
+function refresh_oeuvre() {
+    document.getElementById("title").innerText = curr_oeuvre.title;
+    document.getElementById("picture").src = curr_oeuvre.picture != "" 
+        ? curr_oeuvre.picture : "placeholder.jpg";
+    document.getElementById("rating").innerText = curr_oeuvre.rating;
+}
+
+async function get_reco() {
+    const reco_response = await fetch(back_url+"/reco", {
+        method: "POST",
+        body: JSON.stringify(document.getElementById("media-select").value),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem('jwt')
+        }
+    });
+    if (reco_response.status == 401) {
+        window.location.href = "/login";
+    }
+    curr_oeuvre = await reco_response.json();
+    refresh_oeuvre();
+}
+
+async function rate_oeuvre(rating) {
+    const reco_response = await fetch(back_url+"/rate", {
+        method: "POST",
+        body: JSON.stringify({oeuvre_id: curr_oeuvre.id, rating: rating}),
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem('jwt')
+        }
+    });
+    if (reco_response.status == 401) {
+        window.location.href = "/login";
+    }
+    curr_oeuvre = await reco_response.json();
+    refresh_oeuvre();
+}
---
+++ login.js
@@ -0,0 +1,60 @@
+const back_url = "http://127.0.0.1:8000";
+let username_valid = false;
+let password_valid = false;
+
+window.onload = async function() {
+    check_username_valid();
+    check_password_valid();
+};
+
+function toggle_buttons(enabled) {
+    document.getElementById("login").disabled = !enabled;
+    document.getElementById("signup").disabled = !enabled;
+}
+
+function check_username_valid() {
+    username_valid = document.getElementById("username").value.length >= 3;
+    toggle_buttons(username_valid && password_valid);
+}
+
+function check_password_valid() {
+    password_valid = document.getElementById("password").value.length >= 8;
+    toggle_buttons(username_valid && password_valid);
+}
+
+function credentials() {
+    return {
+        username: document.getElementById("username").value,
+        password: document.getElementById("username").value
+    };
+}
+
+async function login() {
+    const response = await fetch(back_url+"/login", {
+        method: "POST",
+        body: JSON.stringify(credentials()),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+    if (response.ok) {
+        let jwt = await response.text();
+        localStorage.setItem('jwt', jwt);
+        window.location.href = "/";
+    }
+}
+
+async function signup() {
+    const response = await fetch(back_url+"/signup", {
+        method: "POST",
+        body: JSON.stringify(credentials()),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+    if (response.ok) {
+        let jwt = await response.text();
+        localStorage.setItem('jwt', jwt);
+        window.location.href = "/";
+    }
+}
